f15912c342a8fb136281f9b92093eeda552a0a80
Animated <chrisjnassif@gmail.com>
2023-12-16 02:04:23 -0500

    updating my work/ Misc

--- AirLib/include/vehicles/multirotor/RotorActuator.hpp
+++ AirLib/include/vehicles/multirotor/RotorActuator.hpp
@@ -121,9 +121,9 @@ namespace airlib
             output.control_signal_input = control_signal_filter.getInput();
             output.control_signal_filtered = control_signal_filter.getOutput();
             //see relationship of rotation speed with thrust: http://physics.stackexchange.com/a/32013/14061
-            output.speed = sqrt(output.control_signal_filtered * params.max_speed_square);
-            output.thrust = output.control_signal_filtered * params.max_thrust;
-            output.torque_scaler = output.control_signal_filtered * params.max_torque * static_cast<int>(turning_direction);
+            output.speed = sqrt(output.control_signal_input * params.max_speed_square);
+            output.thrust = output.control_signal_input * params.max_thrust;
+            output.torque_scaler = output.control_signal_input * params.max_torque * static_cast<int>(turning_direction);
             output.turning_direction = turning_direction;
         }
 
