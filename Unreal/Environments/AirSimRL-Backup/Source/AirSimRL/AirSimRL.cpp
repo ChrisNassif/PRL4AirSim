c6e22f287ff0dc13a9b5cbfa25c3c7a0943f22a1
Chris Nassif <chrisjnassif@gmail.com>
2024-06-15 00:16:41 -0400

    Final

--- Unreal/Environments/AirSimRL-Backup/Source/AirSimRL/AirSimRL.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AirSimRL.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AirSimRL, "AirSimRL" );
